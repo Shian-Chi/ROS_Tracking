22135dab18cf2e85fd16e608c1114dd9e649e2ba
SianCi <siancicheng@gmail.com>
2024-05-20 07:52:26 -0700

    fixed code

--- trackDetect.py
+++ trackDetect.py
@@ -1,3 +1,4 @@
+import multiprocessing.queues
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import ReliabilityPolicy, QoSProfile
@@ -5,6 +6,8 @@ from rclpy.executors import MultiThreadedExecutor
 from sensor_msgs.msg import NavSatFix, Imu
 from mavros_msgs.msg import Altitude
 from transforms3d import euler
+from tutorial_interfaces.msg import Img, Bbox
+# from mavros_msgs.msg import  Bbox, Img
 
 import time
 import math
@@ -23,10 +26,11 @@ from pid.pid import PID_Ctrl
 from pid.parameter import Parameters
 from pid.motor import motorCtrl
 from pid.position import verticalTargetPositioning
+
 import threading
 
-# from tutorial_interfaces.msg import Img, Bbox
-from mavros_msgs.msg import  Bbox, Img 
+# ROS settings
+rclpy.init()
 
 pub_img = {"first_detect": False,
            "second_detect": False,
@@ -41,49 +45,18 @@ pub_img = {"first_detect": False,
            }
 
 pub_bbox = {'x0': 0,
-        'x1': 0,
-        'y0': 0,
-        'y1': 0
-        }
+            'x1': 0,
+            'y0': 0,
+            'y1': 0
+            }
 
-rclpy.init()
 
 para = Parameters()
 pid = PID_Ctrl()
 position = verticalTargetPositioning()
 
-
-def delay(s: int):
-    s += 1
-    for i in range(1, s):
-        print(f"{i}s")
-        time.sleep(1)
-
-
-def print_detection_info(s, detect_count, end_inference, start_inference, end_nms):
-    inferenceTime = 1E3 * (end_inference - start_inference)
-    NMS_Time = 1E3 * (end_nms - end_inference)
-    total_time = inferenceTime + NMS_Time
-    fps = 1E3 / total_time
-    print("success detect count:", detect_count)
-    print(f"{s}Done. ({inferenceTime:.1f}ms) Inference, ({NMS_Time:.1f}ms) NMS, FPS:{fps:.1f}\n")
-
-
-def bbox_filter(xyxy0, xyxy1):
-    c0 = [((xyxy0[0] + xyxy0[2]) / 2), ((xyxy0[1] + xyxy0[3]) / 2)]
-    c1 = [((xyxy1[0] + xyxy1[2]) / 2), ((xyxy1[1] + xyxy1[3]) / 2)]
-
-    dis = math.sqrt(((c1[0] - c0[0])**2) + ((c1[1] - c1[1])**2))
-    
-    return dis <= 256
-
-
-def radian_conv_degree(Radian):
-    return ((Radian / math.pi) * 180)
-
-
 yaw = motorCtrl(1, 0, 90)
-delay(3)
+time.sleep(2)
 pitch = motorCtrl(2, 0, 45)
 
 def motorPID_Ctrl(frameCenter_X, frameCenter_Y):
@@ -182,10 +155,10 @@ class MinimalSubscriber(Node):
                                            msg.orientation.x,
                                            msg.orientation.y,
                                            msg.orientation.z])
-        self.pithch = radian_conv_degree(ned_euler_data[0])
-        self.roll = radian_conv_degree(ned_euler_data[1])
-        self.yaw = radian_conv_degree(ned_euler_data[2])
-
+        self.pithch = ned_euler_data[0] / math.pi* 180
+        self.roll = ned_euler_data[1] / math.pi* 180
+        self.yaw = ned_euler_data[2] / math.pi* 180
+   
     def getImuPitch(self):
         return self.pitch
 
@@ -206,8 +179,8 @@ class MinimalSubscriber(Node):
 
     def getAltitude(self):
         return self.gps_altitude
-
-
+    
+    
 pub = MinimalPublisher()
 sub = MinimalSubscriber()
 
@@ -217,6 +190,71 @@ def _spinThread(pub, sub):
     executor.add_node(pub)
     executor.add_node(sub)
     executor.spin()
+
+
+def get_RTK_position():
+    return sub.getLatitude(), sub.getLongitude(), sub.getAltitude()
+
+# Record location
+moveLatList = []
+moveLonList = []
+moveAltList = []
+
+# Add position
+def appendPos():
+    la, lo, al = get_RTK_position()
+    moveLatList.append(la)
+    moveLonList.append(lo)
+    moveAltList.append(al)
+
+# Get first position
+appendPos()
+threeMetersStatus = False
+
+# Calculate straight line distance
+def distance(x0: 0.0, y0: 0.0, z0: 0.0, x1: 0.0, y1: 0.0, z1: 0.0, x2: 0.0, y2: 0.0, z2: 0.0):
+    return math.sqrt(((x1 - x0)**2) + ((y1 - y0)**2) + ((z1 - z0)**2))
+
+
+def haversine(lon1, lat1, lon2, lat2, alt1=0.0, alt2=0.0):
+    # Convert angles to radians
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+    
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    
+    r = 6371000  # Radius of the Earth in meters
+    
+    # Horizontal distance
+    horizontal_distance = c * r
+    
+    # Consider the height difference
+    vertical_distance = alt2 - alt1
+    total_distance = math.sqrt(horizontal_distance**2 + vertical_distance**2)
+    
+    return total_distance
+
+class timerTask(Node):
+    def __init__(self):
+        self.betweenStatus = False
+        self.gpsPosition = [],[]
+        self.gimbalAngle = [],[]
+        self.img_timer = self.create_timer(1, self.betweenDis)
+        
+        self.lat, self.lon, self.alt = 0.0, 0.0, 0.0
+        
+    def betweenDis(self):
+        global threeMetersStatus
+        self.alt, self.lon, self.alt = get_RTK_position()
+        p = len(moveLonList)-1 # get the last piece of data's location
+        dis = haversine(lon1=moveLonList[p], lat1=moveLatList[p], alt1=moveAltList[p], \
+                        lon2=self.lon, lat2=self.lat, alt2=self.alt) 
+        threeMetersStatus = False if dis <= 3.0 else True
+        if threeMetersStatus:
+            appendPos()
 
 
 def update_position_data():
@@ -241,25 +279,47 @@ def secondDetect():
 
     tla, tlo = position.groundTargetPostion()
     pub_img["target_longitude"], pub_img["target_latitude"], pub_img["third_detect"], pub_img["camera_center"] = tlo, tla, True, False
-    delay(2)
+    time.sleep(2)
+
 
 def firstDetect():
     pub_img["send_info"] = pub_img["second_detect"] = True
-    
-    print("camera_center = False")    
+
+    print("camera_center = False")
     pub_img["target_longitude"], pub_img["target_latitude"], pub_img["camera_center"] = sub.getLongitude(), sub.getLatitude(), False
-    
-    update_position_data() # Update GPS, IMU, Gimbal data
-    delay(2) # Delay 2s
-    
-    
-def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, device='', view_img=False, nosave=False, classes=None, agnostic_nms=False, augment=False, \
-    project='runs/detect', name='exp', exist_ok=False, no_trace=False, save_txt=False):
 
-    source, weights, view_img, imgsz, trace = source, weights, view_img, img_size, not no_trace
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    update_position_data()  # Update GPS, IMU, Gimbal data
+    time.sleep(2)  # Delay 2s
+
+
+def print_detection_info(s, detect_count, end_inference, start_inference, end_nms):
+    inferenceTime = 1E3 * (end_inference - start_inference)
+    NMS_Time = 1E3 * (end_nms - end_inference)
+    total_time = inferenceTime + NMS_Time
+    fps = 1E3 / total_time
+    print("success detect count:", detect_count)
+    print(f"{s}Done. ({inferenceTime:.1f}ms) Inference, ({NMS_Time:.1f}ms) NMS, FPS:{fps:.1f}\n")
+
+
+def bbox_filter(xyxy0, xyxy1):
+    c0 = [((xyxy0[0] + xyxy0[2]) / 2), ((xyxy0[1] + xyxy0[3]) / 2)]
+    c1 = [((xyxy1[0] + xyxy1[2]) / 2), ((xyxy1[1] + xyxy1[3]) / 2)]
+
+    dis = distance(c1[0], c0[0], c1[1], c1[1])
+    return dis <= 256
+
+
+def txt_save(p, xyxy, conf, cls):
+    line = (cls, *xyxy, conf)  # label format
+    with open(p + '.txt', 'a') as f:
+        f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+
+def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, device='', view_img=False, classes=None, agnostic_nms=False, augment=False,
+           project='runs/detect', name='exp', exist_ok=False, no_trace=False, save_txt=False):
+
+    source, weights, view_img, imgsz, trace = source, weights, check_imshow(), img_size, not no_trace
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
-
     # Directories
     save_dir = Path(increment_path(Path(project) / name, exist_ok=exist_ok))  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
@@ -267,7 +327,6 @@ def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, devic
     # Initialize
     set_logging()
     device = select_device(device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
     model = attempt_load(weights, map_location=device)  # load FP32 model
@@ -277,8 +336,7 @@ def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, devic
     if trace:
         model = TracedModel(model, device, img_size)
 
-    if half:
-        model.half()  # to FP16
+    model.half()  # to FP16
 
     # Set Dataloader
     vid_path, vid_writer = None, None
@@ -291,20 +349,20 @@ def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, devic
     colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
 
     sequentialHits = 0  # The number of consecutive target detections
     sequentialHits_status = 0
     bbox_filter_status = False
-    
+
     # record xyxy position
-    xyxy_previous = [0,0,0,0]
-    
+    xyxy_previous = [0, 0, 0, 0]
+    xyxy_current = [0, 0, 0, 0]
+
     t0 = time.time()
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
-        img = img.half() # uint8 to fp16
+        img = img.half()  # uint8 to fp16
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
@@ -325,14 +383,13 @@ def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, devic
             max_conf = -1  # Variable to store the maximum confidence value
             max_xyxy = None  # Variable to store the xyxy with the maximum confidence
             detectFlag = False
-            
+
             if webcam:
                 p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
 
             p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            
+
             if len(det):
                 detectFlag = True
                 
@@ -350,92 +407,29 @@ def detect(weights, source, img_size=640, conf_thres=0.25, iou_thres=0.45, devic
                     if conf > max_conf:
                         max_conf = conf
                         max_xyxy = xyxy
-                        
-                    if save_txt:  # Write to file
-                        line = (cls, *max_xyxy, conf) # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                    
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-                    
-                
-                xyxy_current = np.array([t.item() for t in max_xyxy], dtype='i4')
-                
-                # Tracking and bbox enable condition
-                """
-                if sequentialHits > 4:
-                    bbox_filter_status = bbox_filter(xyxy_current, xyxy_previous)
-                    if  bbox_filter_status:
-                        pub_bbox["x0"], pub_bbox['y0'], pub_bbox['x1'], pub_bbox["y1"] = xyxy_current
-                        print("bbox_filter_status is True")
-                
-                xyxy_previous = xyxy_current.copy()
-                print(f"current:{xyxy_current}, previous:{xyxy_previous}")
-            else:
-                pub_bbox["x0"] = pub_bbox['y0'] = pub_bbox['x1'] = pub_bbox["y1"] = 0
-                """
-                
-            # Stream results
-            
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-            
 
-            # Save results (image with detections)
-            
-            """
-            if save_img:
-                if dataset.mode == 'stream' or dataset.mode == 'video':
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                            save_path += '.mp4'
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer.write(im0)
-            """
-            
-        
+                    if save_txt:  # Write to file
+                        txt_save(txt_path, max_xyxy, max_conf)
+
+                    if view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        plot_one_box(max_xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+
+                xyxy_current = np.array([t.item() for t in max_xyxy], dtype='i4')
+
+            # Stream results
+            if view_img:
+                cv2.imshow(name, im0)
+                cv2.waitKey(1)  # 1 millisecond
+
         sequentialHits = sequentialHits + 1 if detectFlag else 0
         sequentialHitsStatus = sequentialHits > 4
-        
+
         print_detection_info(s, sequentialHits, t2, t1, t3)
-        
+
         # tracking
-        # if bbox_filter_status:
-        #     pub_img["camera_center"] = PID(max_xyxy)
-        pub_img["camera_center"] = PID(max_xyxy)
-        
-        if pub_img["second_detect"] == True and pub_img["hold_status"]:
-            print("second detect")
-            # Continue detection after descending five meters
-            if sequentialHits_status == 1 and sequentialHitsStatus:  # Detect target for the second time
-                if pub_img["camera_center"] :
-                    secondDetect()           
-                    while not pub_img["hold_status"]:
-                        pub_img["send_info"] = False
-                        sequentialHits_status = 2
-
-        else:  # first_detect == False
-            # Target detected for the first time and Aim at targets
-            if sequentialHits_status == 0 and sequentialHitsStatus:  # Detect target for the first time
-                pub_img["first_detect"] = True
-                print("first detect")
-
-            if pub_img["camera_center"] and pub_img["hold_status"]:  # target centered and drone is hold
-                firstDetect()
-                while not sub.getHold():
-                    pub_img["send_info"] = False
-                    sequentialHits_status = 1
+        if bbox_filter:
+            pub_img["camera_center"] = PID(max_xyxy)
 
 
 def main():
@@ -445,13 +439,12 @@ def main():
 
     # Settings directly specified here
     weights = 'landpad20140411.pt'             # Model weights file path
-    source = 'rtsp://0.0.0.0:8080/test'       # Data source path
+    source = 'rtsp://127.0.0.2:8080/test'       # Data source path
     img_size = 640                    # Image size for inference
     conf_thres = 0.25                 # Object confidence threshold
     iou_thres = 0.4                  # IOU threshold for NMS
     device = '0'                       # Device to run the inference on, '' for auto-select
-    view_img = True                   # Whether to display images during processing
-    nosave = False                    # Whether not to save images/videos
+    view_img = not True                   # Whether to display images during processing
     # Specific classes to detect, None means detect all classes
     classes = None
     agnostic_nms = False              # Apply class-agnostic NMS
@@ -464,7 +457,7 @@ def main():
     # Call the detect function with all the specified settings
     with torch.no_grad():
         detect(weights, source, img_size, conf_thres, iou_thres, device, view_img,
-               nosave, classes, agnostic_nms, augment, project, name, exist_ok, no_trace)
+               classes, agnostic_nms, augment, project, name, exist_ok, no_trace)
 
 
 if __name__ == '__main__':
