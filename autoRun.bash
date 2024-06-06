1b78d63503a25298cb77cec7f130897780a91755
SianCi <siancicheng@gmail.com>
2024-06-06 01:36:48 -0700

    open code.py

--- autoRun.bash
+++ autoRun.bash
@@ -12,15 +12,15 @@ echo "123456789" | sudo -S chmod 666 /dev/ttyPixhawk
 
 echo "123456789" | sudo -S chmod 666 /dev/ttyTHS0 
 
-# ros2 run mavros mavros_node --ros-args --param fcu_url:=serial:///dev/ttyPixhawk &
-# sleep 20
+ros2 run mavros mavros_node --ros-args --param fcu_url:=serial:///dev/ttyPixhawk &
+sleep 20
 
 # 使用 pipenv run 執行 Python 腳本
-# pipenv run python3 /home/ubuntu/yolo/yolo_tracking/drone_ROS2.py &
-# sleep 10
+pipenv run python3 /home/ubuntu/yolo/yolo_tracking/drone_ROS2.py &
+sleep 20
 
-# pipenv run python3 /home/ubuntu/yolo/yolo_tracking/drone_PWCL_new.py &
-# sleep 10
+pipenv run python3 /home/ubuntu/yolo/yolo_tracking/drone_PWCL_new.py &
+sleep 20
 
 pipenv run python3 /home/ubuntu/yolo/yolo_tracking/trackDetect.py 
 
