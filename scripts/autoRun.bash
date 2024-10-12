30c2a02d14645fb73e3a08f36a87204360306bc3
Shian-Chi <siancicheng@gmail.com>
2024-10-12 02:14:42 -0700

    fix

--- scripts/autoRun.bash
+++ scripts/autoRun.bash
@@ -12,21 +12,28 @@ echo "123456789" | sudo -S chmod 666 /dev/ttyPixhawk
 
 echo "123456789" | sudo -S chmod 666 /dev/ttyTHS0 
 
-source ./scripts/lidar.bash
-
 ros2 run mavros mavros_node --ros-args --param fcu_url:=serial:///dev/ttyPixhawk &
 sleep 20
 
-# 使用 pipenv run 執行 Python 腳本
-pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/Node/lidar/drone_lidar.py &
+nohup python3 /home/ubuntu/CSI_Camera_H265/CSI_H265.py &
+sleep 3
+
+nohup python3 /home/ubuntu/CSI_Camera_H265/recording_timeout.py &
+sleep 3
+
+# Flight
+python3 /home/ubuntu/yolo/yolo_tracking_v2/drone_landing_ROS2.py &
 sleep 20
 
-pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/Node//flight/drone_ROS2.py &
+# PWCL
+python3 /home/ubuntu/yolo/yolo_tracking_v2/drone_PWCL_new.py &
 sleep 20
 
-pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/Node/PWCL/drone_PWCL_new.py &
-sleep 20
+#Lidar
+#nohup python3 /home/ubuntu/yolo/yolo_tracking_v2/lidar_alt.py &
+#sleep 3
 
-pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/trackDetect_v2.py 
+# YOLO
+pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/trackDetect_2.py &
 
-
+sleep 1000000000
