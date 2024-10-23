6ede3e74074da36a0f4a8e00bc3d6b8d38cfb7c7
Shian-Chi <siancicheng@gmail.com>
2024-10-23 00:37:02 -0700

    first commit

--- ctrl/gimbal_PID.py
+++ ctrl/gimbal_PID.py
@@ -4,10 +4,9 @@ from rclpy.executors import MultiThreadedExecutor
 
 import sys
 import signal
-from pid.PID_Calc import PID_Ctrl
-from pid.motor import motorCtrl
+from ctrl.pid.PID_Calc import PID_Ctrl
+from ctrl.pid.motor import motorCtrl
 from tutorial_interfaces.msg import Bbox
-
 
 pid = PID_Ctrl()
 
@@ -30,15 +29,14 @@ class GimbalSubscriber(Node):
         self.y1 = 0
 
     def listener_callback(self, msg):
-        self.detect = msg.detect
-        self.ID = msg.class_id
-        self.conf = msg.confidence
+
         self.x0 = msg.x0
         self.y0 = msg.y0
         self.x1 = msg.x1
         self.y1 = msg.y1
 
-    def get_bbox_position(self):
+    def get_bbox(self):
+        print(f"get_bbox: {self.detect}")
         return self.x0, self.y0, self.x1, self.y1
 
 
@@ -51,30 +49,32 @@ class GimbalTimerTask(Node):
         else:
             self.sub_para = GimbalSubscriber()
             
-        gimal_period = 1/20
-        self.gimbal_task = self.create_timer(gimal_period, self.gimdal_ctrl)
+        gimbal_period = 1 / 21  # 21 Hz
+        self.gimbal_task = self.create_timer(gimbal_period, self.gimdal_ctrl)
 
         self.bbox_center = False
+        self.l_xyxy = [0, 0, 0, 0]
 
     def gimdal_ctrl(self):
-        if self.sub_para.detect:
-            x0, y0, x1, y1 = self.sub_para.get_bbox_position()
-            x, y = (x0 + x1) / 2, (y0 + y1) / 2
-            pidErr = pid.pid_run(x, y)
+        m_flag1 = m_flag2 =False  # Ensure flags are initialized
+        xyxy = list(self.sub_para.get_bbox())
 
-            # Motor rotation
-            if abs(pidErr[0]) != 0:
-                yaw.incrementTurnVal(int(pidErr[0]*100))
-            else:
-                m_flag1 = True
+        x, y = (xyxy[0] + xyxy[2]) / 2, (xyxy[1] + xyxy[3]) / 2
+        pidErr = pid.pid_run(x, y)
+        print(f"PID ERR: {pidErr}")
+        # Motor rotation
+        if abs(pidErr[0]) != 0:
+            yaw.incrementTurnVal(int(pidErr[0] * 100))
+        else:
+            m_flag1 = True
 
-            if abs(pidErr[1]) != 0:
-                pitch.incrementTurnVal(int(pidErr[1]*100))
-            else:
-                m_flag2 = True
+        if abs(pidErr[1]) != 0:
+            pitch.incrementTurnVal(int(pidErr[1] * 100))
+        else:
+            m_flag2 = True
 
-            self.bbox_center = m_flag1 and m_flag2
-
+        self.bbox_center = m_flag1 and m_flag2
+        xyxy = self.l_xyxy
 
 def spinThread(sub, task):
     executor = MultiThreadedExecutor()
