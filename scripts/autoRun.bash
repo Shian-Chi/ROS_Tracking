e2bf4f8a008cf96e6280427abd88a3a69d2e5520
Shian-Chi <siancicheng@gmail.com>
2024-11-11 08:47:48 -0800

    RTSP to HTTP streaming

--- scripts/autoRun.bash
+++ scripts/autoRun.bash
@@ -4,18 +4,23 @@ cd /home/ubuntu/yolo/yolo_tracking_v2/
 source ./install/setup.bash
 
 
-echo "123456789" | sudo -S chmod 666 /dev/ttyRTK
+echo "123456789" | sudo -S chmod 777 /dev/ttyRTK
 
-echo "123456789" | sudo -S chmod 666 /dev/ttyXbee
+echo "123456789" | sudo -S chmod 777 /dev/ttyXbee
 
-echo "123456789" | sudo -S chmod 666 /dev/ttyPixhawk
+echo "123456789" | sudo -S chmod 777 /dev/ttyPixhawk 
 
-echo "123456789" | sudo -S chmod 666 /dev/ttyTHS0 
+echo "123456789" | sudo -S chmod 777 "/dev/ttyTHS0"
+
+echo "123456789" | sudo -S i2cdetect -y 8
 
 ros2 run mavros mavros_node --ros-args --param fcu_url:=serial:///dev/ttyPixhawk &
 sleep 20
 
 nohup python3 /home/ubuntu/CSI_Camera_H265/CSI_H265.py &
+sleep 3
+
+nohup python /home/ubuntu/home/bbb.py
 sleep 3
 
 nohup python3 /home/ubuntu/CSI_Camera_H265/recording_timeout.py &
@@ -30,10 +35,10 @@ python3 /home/ubuntu/yolo/yolo_tracking_v2/PWCL/drone_PWCL_new.py &
 sleep 20
 
 #Lidar
-nohup python3 /home/ubuntu/yolo/yolo_tracking_v2/lidar_alt.py &
+#nohup python3 /home/ubuntu/yolo/yolo_tracking_v2/lidar_alt.py &
 #sleep 3
 
 # YOLO
-nohup pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/detect/trackDetect_2.py &
+pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/detect/trackDetect_2.py &
 
-sleep 1000000000
+sleep infinity
