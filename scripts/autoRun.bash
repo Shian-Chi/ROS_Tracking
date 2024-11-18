6b9cb97ea2f467996e23ea2a385ba37733c11a37
Shian-Chi <siancicheng@gmail.com>
2024-11-17 22:48:19 -0800

    test code

--- scripts/autoRun.bash
+++ scripts/autoRun.bash
@@ -10,35 +10,35 @@ echo "123456789" | sudo -S chmod 777 /dev/ttyXbee
 
 echo "123456789" | sudo -S chmod 777 /dev/ttyPixhawk 
 
-echo "123456789" | sudo -S chmod 777 "/dev/ttyTHS0"
+#echo "123456789" | sudo -S chmod 777 "/dev/ttyTHS0"
 
 echo "123456789" | sudo -S i2cdetect -y 8
 
 ros2 run mavros mavros_node --ros-args --param fcu_url:=serial:///dev/ttyPixhawk &
-sleep 20
+#sleep 20
 
-nohup python3 /home/ubuntu/CSI_Camera_H265/CSI_H265.py &
-sleep 3
+#nohup python3 /home/ubuntu/CSI_Camera_H265/CSI_H265.py &
+#sleep 3
 
-nohup python /home/ubuntu/home/bbb.py
-sleep 3
+#nohup python /home/ubuntu/home/bbb.py
+#sleep 3
 
-nohup python3 /home/ubuntu/CSI_Camera_H265/recording_timeout.py &
-sleep 3
+#nohup python3 /home/ubuntu/CSI_Camera_H265/recording_timeout.py &
+#sleep 3
 
-# Flight
-python3 /home/ubuntu/yolo/yolo_tracking_v2/flight/drone_landing_ROS2.py &
-sleep 20
+## Flight
+#python3 /home/ubuntu/yolo/yolo_tracking_v2/flight/drone_landing_ROS2.py &
+#sleep 20
 
 # PWCL
-python3 /home/ubuntu/yolo/yolo_tracking_v2/PWCL/drone_PWCL_new.py &
-sleep 20
+#python3 /home/ubuntu/yolo/yolo_tracking_v2/PWCL/drone_PWCL_new.py &
+#sleep 20
 
 #Lidar
 #nohup python3 /home/ubuntu/yolo/yolo_tracking_v2/lidar_alt.py &
 #sleep 3
 
 # YOLO
-pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/detect/trackDetect_2.py &
+#pipenv run python3 /home/ubuntu/yolo/yolo_tracking_v2/detect/trackDetect_2.py &
 
 sleep infinity
