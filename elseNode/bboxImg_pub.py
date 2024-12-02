3ffab43de975c57de476b2acb8477fd79b1d984d
Shian-Chi <siancicheng@gmail.com>
2024-12-01 21:45:09 -0800

    fix

--- elseNode/bboxImg_pub.py
+++ elseNode/bboxImg_pub.py
@@ -53,6 +53,7 @@ def signal_handler(sig, frame):
 
 class MinimalTasks(Node):
     def __init__(self):
+        super().__init__("BboxImg Tasks")
         CV_time = 1/30
         self.CV_timer = self.create_timer(CV_time, self.bboxImg_callback)  # 每0.033秒發布一次圖像
 
