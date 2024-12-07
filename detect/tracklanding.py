5b8997aa56b2e54def6754f3307db068024fbec5
Shian-Chi <siancicheng@gmail.com>
2024-12-07 04:55:22 -0800

    use new dataset function

--- detect/tracklanding.py
+++ detect/tracklanding.py
@@ -6,7 +6,7 @@ import torch
 import torch.backends.cudnn as cudnn
 import numpy as np
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams
+from utils.datasets import letterbox
 from utils.general import check_img_size, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, set_logging
 from utils.plots import plot_one_box
