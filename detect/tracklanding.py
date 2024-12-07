347350eecfb081128bcb7bbcd15b6ec381beb290
Shian-Chi <siancicheng@gmail.com>
2024-12-07 04:53:58 -0800

    use new dataset function

--- detect/tracklanding.py
+++ detect/tracklanding.py
@@ -448,7 +448,7 @@ def main(args=None):
     # YOLO
     # Settings directly specified here
     weights = 'landpad20240522.pt'                                              # Model weights file path
-    source ='rtsp://127.0.0.' + str(np.random.randint(1,256)) + ':8080/video_feed'    # Data source path
+    source = 'CSI_Camera'                                                       # Data source path
     # Data source path
     img_size = 640                                                              # Image size for inference
     conf_thres = 0.4                                                            # Object confidence threshold
